2f7536f9989daee385c97cf6e29a63da3014901f
guru <gurusamanthula@gmail.com>
2020-12-17 15:33:15 +0530

    first commit

---
+++ src/test/java/qaclicksacademy/AppTest.java
@@ -0,0 +1,15 @@
+package qaclicksacademy;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+	@Test
+	public void personalMobile() {
+		System.out.println("personalMobile");
+	}
+
+	@Test
+	public void personalMobileExclude() {
+		System.out.println("personalMobileExclude");
+	}
+}
---
+++ src/test/java/qaclicksacademy/SeleniumTest.java
@@ -0,0 +1,15 @@
+package qaclicksacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void Mobile() {
+		System.out.println("Mobile");
+	}
+
+	@Test
+	public void MobileExclude() {
+		System.out.println("MobileExclude");
+	}
+}
